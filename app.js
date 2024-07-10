c8bbe9c2c8690a6c811186624f93f456e237e51d
Piyush <upadhyaypiyush1234@gmail.com>
2024-07-10 14:08:41 +0530

    message

--- app.js
+++ app.js
@@ -1,41 +1,39 @@
-import express from "express"
-import dotenv from "dotenv"
-import path from "path"
-import { fileURLToPath } from 'url';
-import router from "./src/routes/index.js"
-import connectDb from "./src/helpers/db/db.js"
-import { errorHandler } from "./src/middlewares/error.handler.js"
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+import router from "./src/routes/index.js";
+import connectDb from "./src/helpers/db/db.js";
+import { errorHandler } from "./src/middlewares/error.handler.js";
 
-const app = express()
+const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config(
-    {
-        path: "./src/config/config.env"
-    }
-)
-connectDb()
-const port = process.env.PORT || 3029
+dotenv.config({
+  path: "./src/config/config.env",
+});
+connectDb();
+const port = process.env.PORT || 3029;
 
 // Express - Body Middleware
-app.use(express.json())
+app.use(express.json());
 
 // Router Middlewares
-app.use("/api", router)
+app.use("/api", router);
 
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-app.get("*", (req, res) => {
-    res.status(404).json(
-        {
-            err: "This page doesn't exist."
-        }
-    )
-})
+// app.get("*", (req, res) => {
+//     res.status(404).json(
+//         {
+//             err: "This page doesn't exist."
+//         }
+//     )
+// })
 
 app.listen(port, () => {
-    console.log(`The server was started on ${port} port.`)
-})
+  console.log(`The server was started on ${port} port.`);
+});
