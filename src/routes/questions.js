ee651bc27c0269a813905393cc3398c3456f4d86
Piyush <upadhyaypiyush1234@gmail.com>
2024-07-10 13:54:10 +0530

    message

--- src/routes/questions.js
+++ src/routes/questions.js
@@ -1,38 +1,39 @@
-import express from "express";
+import express from "express"
+import { getAccessToRoute, getQuestionOwnerAccess } from "../middlewares/auth.middleware.js"
+import { checkQuesitonExist } from "../helpers/db/db.error.helpers.js"
 import {
-  getAccessToRoute,
-  getQuestionOwnerAccess,
-} from "../middlewares/auth.middleware.js";
-import { checkQuesitonExist } from "../helpers/db/db.error.helpers.js";
-import {
-  getAllQuestions,
-  getQuestion,
-  addQuestion,
-  updateQuestion,
-  deleteQuestion,
-  favQuestion,
-  unFavQuestion,
-} from "../controllers/quesitons.controller.js";
-import answer from "../routes/answer.js";
+    getAllQuestions, getQuestion,
+    addQuestion, updateQuestion,
+    deleteQuestion, favQuestion,
+    unFavQuestion
+} from "../controllers/quesitons.controller.js"
+import answer from "../routes/answer.js"
 
-const router = express.Router();
+const router = express.Router()
 
-// router.use(getAccessToRoute)
-router.get("/allQuestions", getAllQuestions);
-router.get("/getQuestion/:id", checkQuesitonExist, getQuestion);
-router.post("/ask", addQuestion);
+router.use(getAccessToRoute)
+router.get("/allQuestions", getAllQuestions)
+router.get("/getQuestion/:id", checkQuesitonExist, getQuestion)
+router.post("/ask", addQuestion)
 router.put(
-  "/updateQuestion/:id",
-  [checkQuesitonExist, getQuestionOwnerAccess],
-  updateQuestion
-);
-router.delete(
-  "/deleteQuestion/:id",
-  [checkQuesitonExist, getQuestionOwnerAccess],
-  deleteQuestion
-);
-router.get("/favQuestion/:id", checkQuesitonExist, favQuestion);
-router.get("/unFavQuestion/:id", checkQuesitonExist, unFavQuestion);
-router.use("/:id/answers", checkQuesitonExist, answer);
+    "/updateQuestion/:id",
+    [checkQuesitonExist,
+        getQuestionOwnerAccess],
+    updateQuestion
+)
+router.delete("/deleteQuestion/:id",
+    [checkQuesitonExist,
+        getQuestionOwnerAccess],
+    deleteQuestion
+)
+router.get("/favQuestion/:id",
+    checkQuesitonExist,
+    favQuestion
+)
+router.get("/unFavQuestion/:id",
+    checkQuesitonExist,
+    unFavQuestion
+)
+router.use("/:id/answers", checkQuesitonExist, answer)
 
-export default router;
+export default router
